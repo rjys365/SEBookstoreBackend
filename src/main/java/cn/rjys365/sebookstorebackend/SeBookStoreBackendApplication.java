2b51f03253622903b82e345f22f9856d6b566607
rjys365 <rjys365@hotmail.com>
2023-04-12 12:13:36 +0800

    first commit

---
+++ src/main/java/cn/rjys365/sebookstorebackend/SeBookStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.rjys365.sebookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeBookStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeBookStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/rjys365/sebookstorebackend/SeBookStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.rjys365.sebookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeBookStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
