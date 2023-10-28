bbbf5c4b39c0acc096ca928f3b896ce5db2a9432
Zhang Zhuyue <zhang.zhuyue@sjtu.edu.cn>
2023-10-28 22:05:18 +0800

    initiate Spring Cloud Eureka project

---
+++ SEBookstoreEurekaServer/src/main/java/cn/rjys365/sebookstoreeurekaserver/SeBookstoreEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package cn.rjys365.sebookstoreeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeBookstoreEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeBookstoreEurekaServerApplication.class, args);
+    }
+
+}
