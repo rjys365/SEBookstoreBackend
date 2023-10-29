a85d3ed74b537dbbe28ab606397f988ca5227b66
Zhang Zhuyue <zhang.zhuyue@sjtu.edu.cn>
2023-10-29 12:17:04 +0800

    use Spring Cloud Gateway

--- SEBookstoreEurekaServer/src/main/java/cn/rjys365/sebookstoreeurekaserver/SeBookstoreEurekaServerApplication.java
+++ SEBookstoreEurekaServer/src/main/java/cn/rjys365/sebookstoreeurekaserver/SeBookstoreEurekaServerApplication.java
@@ -2,8 +2,10 @@ package cn.rjys365.sebookstoreeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SeBookstoreEurekaServerApplication {
 
     public static void main(String[] args) {
--- SEBookstoreGateway/src/main/java/cn/rjys365/sebookstoregateway/SeBookstoreGatewayApplication.java
+++ SEBookstoreGateway/src/main/java/cn/rjys365/sebookstoregateway/SeBookstoreGatewayApplication.java
@@ -1,4 +1,4 @@
-package cn.rjys365.sebookstorebackend;
+package cn.rjys365.sebookstoregateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SeBookStoreBackendApplication {
+public class SeBookstoreGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeBookStoreBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeBookstoreGatewayApplication.class, args);
+    }
 
 }
